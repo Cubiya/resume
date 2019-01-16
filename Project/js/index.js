5a555b06103ffc3f7dd88f65163446201ab2d00e
LiWenhua <liwenhua@tedu.cn>
2019-01-16 20:53:58 +0800

    增加gx,lx,qc页面

--- Project/js/index.js
+++ Project/js/index.js
@@ -2,14 +2,12 @@
     /*打开右边导航栏*/ 
     var nav=document.getElementById("head_navbtn")
     nav.onclick=function(){
-        var nav=this;
         var btn=document.getElementsByClassName("nav")[0]
         btn.style.right=`${0}px`;
     }
     /*关闭右边导航栏*/
     var navclose=document.querySelector(".nav>button")
     navclose.onclick=function(){
-        var navclose=this;
         var btn=document.getElementsByClassName("nav")[0]
         btn.style.right=`${-340}px`;
     }
