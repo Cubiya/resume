56b89267e4be7f160a54946be34e3e9dceccfea2
LiWenhua <liwenhua@tedu.cn>
2019-01-22 20:59:15 +0800

    每个页面增加js文件

---
+++ Project/public/js/gx.js
@@ -0,0 +1,9 @@
+$(function(){
+    /******************** 侧边导航 ********************/
+    $("#headnav").click(function(){
+        $(".nav").css("right","0").children(":first")
+        .click(function(){
+            $(".nav").css("right","-340px")
+        })
+    })
+})
